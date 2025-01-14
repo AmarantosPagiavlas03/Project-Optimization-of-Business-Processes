ad066b9f42eaeb296342de891829bdd0820add1e
Eirini <e.aseni@student.vu.nl>
2025-01-14 20:47:34 +0100

    test2

--- dynamic_programming/irene_test.py
+++ dynamic_programming/irene_test.py
@@ -2,7 +2,7 @@ import pandas as pd
 import random
 from datetime import timedelta, datetime
 
-print(12)
+print(123)
 # Sample data for task names, durations, and nurses required
 task_names = ["Admin Check", "Patient Round", "Blood Test", "Medication Dispense"]
 nurses_required = [1, 2, 3, 4]
