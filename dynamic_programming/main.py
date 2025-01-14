f0a402eb91e3a124d2ebc43a77264bd28c674859
AmarantosPagiavlas2003 <a.e.pagiavlas@student.vu.nl>
2025-01-14 18:40:05 +0100

    test

--- dynamic_programming/main.py
+++ dynamic_programming/main.py
@@ -11,7 +11,6 @@ def init_db():
     conn = sqlite3.connect(DB_FILE)
     c = conn.cursor()
     c.execute('''
-    DROP TABLE IF EXISTS tasks
     CREATE TABLE IF NOT EXISTS tasks (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         task_name TEXT NOT NULL,
