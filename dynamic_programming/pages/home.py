943ca7d195369ac0c8c423df9591aa9812f7552c
AmarantosPagiavlas2003 <a.e.pagiavlas@student.vu.nl>
2025-01-24 17:15:59 +0100

    test

--- dynamic_programming/pages/home.py
+++ dynamic_programming/pages/home.py
@@ -14,7 +14,7 @@ def show_home():
 
 
     
-    if st.button("Clear All Tasks"):
+    if st.button("Clear All Tasks1"):
         clear_all("TasksTable2")
         st.success("All tasks have been cleared!")
  
