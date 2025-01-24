63380204b47803041dd5f66136e80e7d76f57ad2
AmarantosPagiavlas2003 <a.e.pagiavlas@student.vu.nl>
2025-01-24 23:48:05 +0100

    test

--- dynamic_programming/main.py
+++ dynamic_programming/main.py
@@ -1285,9 +1285,9 @@ def display_tasks_and_shifts():
 
     # Show empty state if no data
     if tasks_df.empty and shifts_df.empty:
+        st.info("🌟 No tasks or shifts found. Add data to get started!")
         _,_, col, _, _ = st.columns([1, 1, 1,1,1])  # Ratio creates centered middle column
         with col:
-            st.info("🌟 No tasks or shifts found. Add data to get started!")
             st.image("https://cdn-icons-png.flaticon.com/512/7486/7486744.png", width=200)
         return
 
