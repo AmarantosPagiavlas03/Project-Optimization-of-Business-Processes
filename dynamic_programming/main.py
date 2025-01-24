908158eed4d04c9b1035f9974a2b0d5991a07eb9
AmarantosPagiavlas2003 <a.e.pagiavlas@student.vu.nl>
2025-01-24 21:15:04 +0100

    test

--- dynamic_programming/main.py
+++ dynamic_programming/main.py
@@ -1572,7 +1572,7 @@ def main():
         }
         .stButton button:hover {
             transform: scale(1.05);
-            background-color: #2196F3 !important;  
+            /* background-color: #2196F3 !important;  */
         }
         /* Keep the rest of the CSS the same */
         .stDownloadButton button {
