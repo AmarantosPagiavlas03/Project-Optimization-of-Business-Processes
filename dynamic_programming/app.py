b9328adf527a7c7fcfd9c5f9bdb9716953ed40db
AmarantosPagiavlas2003 <a.e.pagiavlas@student.vu.nl>
2025-01-24 15:28:37 +0100

    τεστ

--- dynamic_programming/app.py
+++ dynamic_programming/app.py
@@ -57,13 +57,13 @@ styles = {
         "font-weight": "500"
     },
     "active": {
-        "background-color": "#e6000f",
-        "color": "white",
+        # "background-color": "#e6000f",
+        "color": "e6000f",
         "font-weight": "500",
-        "padding": "20px 24px",  # Increased from 14px
-        "border-radius": "50%",   # Perfect circle
-        "margin": "0 8px",        # Space between circle and other elements
-        "transform": "scale(1.1)" # Slight enlargement
+        # "padding": "20px 24px",  # Increased from 14px
+        # "border-radius": "50%",   # Perfect circle
+        # "margin": "0 8px",        # Space between circle and other elements
+        # "transform": "scale(1.1)" # Slight enlargement
     }
 }
 
