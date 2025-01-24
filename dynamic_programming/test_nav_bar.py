03a561f7d1b31f020ccd616de34017714618a120
AmarantosPagiavlas2003 <a.e.pagiavlas@student.vu.nl>
2025-01-24 13:51:15 +0100

    test

--- dynamic_programming/test_nav_bar.py
+++ dynamic_programming/test_nav_bar.py
@@ -2,5 +2,8 @@ import streamlit as st
 from streamlit_navigation_bar import st_navbar
 
 
-page = st_navbar(["Home", "Documentation", "Examples", "Community", "About"])
+page = st_navbar(
+    pages=["Home", "Library", "Tutorials", "Development", "Download"],
+    options={"use_padding": False}
+)
 st.write(page)
