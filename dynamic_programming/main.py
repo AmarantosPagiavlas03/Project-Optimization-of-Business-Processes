aed302f9d220c5d629a4264fcfe90d2ebb2a08c6
AmarantosPagiavlas2003 <a.e.pagiavlas@student.vu.nl>
2025-01-24 13:03:23 +0100

    test

--- dynamic_programming/main.py
+++ dynamic_programming/main.py
@@ -17,7 +17,7 @@ from streamlit_option_menu import option_menu
 from streamlit_extras.switch_page_button import switch_page
 
 
-
+st.set_page_config(page_title="Home", layout="wide")
 DB_FILE = "tasksv2.db"
 
 
