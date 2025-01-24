a02c7a8e118d12d47bb4ce4e0394be1c4fceba8e
AmarantosPagiavlas2003 <a.e.pagiavlas@student.vu.nl>
2025-01-24 17:08:40 +0100

    test

--- dynamic_programming/pages/home.py
+++ dynamic_programming/pages/home.py
@@ -1,42 +1,30 @@
 import streamlit as st
 from database import  clear_all, insert, insert2
-from forms import task_input_form, shift_input_form, worker_input_form, task_template_download, upload_tasks_excel, shift_template_download, upload_shifts_excel
+from forms import task_input_form, shift_input_form, worker_input_form, task_template_download, upload_tasks_excel, shift_template_download, upload_shifts_excel, task_upload_download_form, shift_upload_download_form
 from dynamic_programming.optimizer import optimize_tasks_with_gurobi 
 from dynamic_programming.visualization import display_tasks_and_shifts
 
 def show_home():
-    # st.set_page_config(page_title="Hospital Scheduler", layout="wide")
+
     # Input forms
     task_input_form()
     shift_input_form()
-    # worker_input_form()
- 
-    # Buttons for example data
- 
+    task_upload_download_form()
+    shift_upload_download_form()
+
+
+    
     if st.button("Clear All Tasks"):
         clear_all("TasksTable2")
-        st.success("All tasks have been cleared!!!")
+        st.success("All tasks have been cleared!")
  
     if st.button("Clear All Shifts"):
         clear_all("ShiftsTable5")
-        st.success("All shifts have been cleared!!!")
+        st.success("All shifts have been cleared!")
 
+ 
 
-
-    # 1. Download Template
-    st.subheader("Download Template")
-    task_template_download()
-
-    st.markdown("---")
-
-    # 2. Upload user file
-    st.subheader("Upload Your Tasks")
-    upload_tasks_excel()
-
-    # if st.button("Clear All Workers"):
-    #     clear_all("Workers")
-    #     st.success("All workers have been cleared!")
-
+ 
  
     st.subheader("Download Example Shift Template")
     shift_template_download()
