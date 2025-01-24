8bf96513152b9736343abfde9ab036280325c106
AmarantosPagiavlas2003 <a.e.pagiavlas@student.vu.nl>
2025-01-24 14:30:47 +0100

    test

--- dynamic_programming/app.py
+++ dynamic_programming/app.py
@@ -3,7 +3,11 @@ import streamlit as st
 from streamlit_navigation_bar import st_navbar
 import pages as pg
 from database import init_db
+import sys
+import os
 
+# Add this at the top of app.py
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 st.set_page_config(initial_sidebar_state="collapsed")
 init_db()
 pages = ["Home", "Contact"]
