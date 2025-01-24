0b2db0e1f15467627bb01859fc07cfb92a35f91c
AmarantosPagiavlas2003 <a.e.pagiavlas@student.vu.nl>
2025-01-24 12:32:31 +0100

    τεστ

--- dynamic_programming/main.py
+++ dynamic_programming/main.py
@@ -1671,17 +1671,33 @@ def footer():
 # ------------------------------------------------------------------
 def main():
     st.set_page_config(page_title="Hospital Scheduler", layout="wide")
-    header()
+    # header()
+    st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+    st.markdown("""
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
+    <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Data Professor</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+        <li class="nav-item active">
+            <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="https://youtube.com/dataprofessor" target="_blank">YouTube</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="https://twitter.com/thedataprof" target="_blank">Twitter</a>
+        </li>
+        </ul>
+    </div>
+    </nav>
+    """, unsafe_allow_html=True)
     
     init_db()
-    st.markdown("""
-    <style>
-    /* Force the sidebar to appear on top */
-    [data-testid="stSidebar"] {
-        z-index: 9999 !important;
-    }
-    </style>
-    """, unsafe_allow_html=True)
+ 
     # Input forms
     task_input_form()
     shift_input_form()
