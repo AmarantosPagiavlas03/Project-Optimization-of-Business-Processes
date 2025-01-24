a515e32bd642ef8dfad59527fe88f1c085beb580
AmarantosPagiavlas2003 <a.e.pagiavlas@student.vu.nl>
2025-01-24 13:33:36 +0100

    test

--- dynamic_programming/main.py
+++ dynamic_programming/main.py
@@ -1527,77 +1527,77 @@ def display_tasks_and_shifts():
 
 
 def main():
-    st.set_page_config(page_title="Hospital Scheduler", layout="wide")
+    # st.set_page_config(page_title="Hospital Scheduler", layout="wide")
     page = st_navbar(["Home", "Analytics", "Contact"])
     st.write(page)
         
-    init_db()
+    # init_db()
  
-    # Input forms
-    task_input_form()
-    shift_input_form()
-    # worker_input_form()
+    # # Input forms
+    # task_input_form()
+    # shift_input_form()
+    # # worker_input_form()
  
-    with st.sidebar:
-        st.markdown("---")  # Add a separator line
-        col1, col2 = st.columns(2)
+    # with st.sidebar:
+    #     st.markdown("---")  # Add a separator line
+    #     col1, col2 = st.columns(2)
 
-        with col1:
-            if st.button("Clear All Tasks"):
-                clear_all("TasksTable2")
-                st.success("All tasks have been cleared!")
+    #     with col1:
+    #         if st.button("Clear All Tasks"):
+    #             clear_all("TasksTable2")
+    #             st.success("All tasks have been cleared!")
 
-        with col2:
-            if st.button("Clear All Shifts"):
-                clear_all("ShiftsTable5")
-                st.success("All shifts have been cleared!")
+    #     with col2:
+    #         if st.button("Clear All Shifts"):
+    #             clear_all("ShiftsTable5")
+    #             st.success("All shifts have been cleared!")
 
-    with st.sidebar.expander("Task Data Import/Export"):
-        # 1. Download Template
-        st.subheader("Download Template")
-        task_template_download()
+    # with st.sidebar.expander("Task Data Import/Export"):
+    #     # 1. Download Template
+    #     st.subheader("Download Template")
+    #     task_template_download()
 
-        st.markdown("---")
+    #     st.markdown("---")
 
-        # 2. Upload user file
-        st.subheader("Upload Your Tasks")
-        upload_tasks_excel()
+    #     # 2. Upload user file
+    #     st.subheader("Upload Your Tasks")
+    #     upload_tasks_excel()
 
-        # if st.button("Clear All Workers"):
-        #     clear_all("Workers")
-        #     st.success("All workers have been cleared!")
+    #     # if st.button("Clear All Workers"):
+    #     #     clear_all("Workers")
+    #     #     st.success("All workers have been cleared!")
 
-    with st.sidebar.expander("Shift Data Import/Export"):
-        st.subheader("Download Example Shift Template")
-        shift_template_download()
+    # with st.sidebar.expander("Shift Data Import/Export"):
+    #     st.subheader("Download Example Shift Template")
+    #     shift_template_download()
 
-        st.markdown("---")
+    #     st.markdown("---")
         
-        st.subheader("Upload Your Shifts File")
-        upload_shifts_excel()
+    #     st.subheader("Upload Your Shifts File")
+    #     upload_shifts_excel()
 
-    # Buttons for example data
-    colA, colB = st.columns(2)
-    with colA:
-        if st.button("Data Example"):
-            insert()
-            st.success("Data Example 1 inserted!")
-    with colB:
-        if st.button("Data Example2"):
-            insert2()
-            st.success("Data Example 2 inserted!")
+    # # Buttons for example data
+    # colA, colB = st.columns(2)
+    # with colA:
+    #     if st.button("Data Example"):
+    #         insert()
+    #         st.success("Data Example 1 inserted!")
+    # with colB:
+    #     if st.button("Data Example2"):
+    #         insert2()
+    #         st.success("Data Example 2 inserted!")
 
-    # First optimization
-    if st.button("Optimize Task Assignment"):
-        optimize_tasks_with_gurobi()
+    # # First optimization
+    # if st.button("Optimize Task Assignment"):
+    #     optimize_tasks_with_gurobi()
 
-    ## Second optimization: Assign workers to shifts
-    # if st.button("Assign Workers to Shifts"):
-    #     optimize_workers_for_shifts()
+    # ## Second optimization: Assign workers to shifts
+    # # if st.button("Assign Workers to Shifts"):
+    # #     optimize_workers_for_shifts()
 
 
-    # Visualization
-    display_tasks_and_shifts()
+    # # Visualization
+    # display_tasks_and_shifts()
 
 
 
