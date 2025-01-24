4ab276cc0090969b0d55504e0da05ea6f521baff
AmarantosPagiavlas2003 <a.e.pagiavlas@student.vu.nl>
2025-01-24 14:11:59 +0100

    test

--- dynamic_programming/test_nav_bar.py
+++ dynamic_programming/test_nav_bar.py
@@ -9,25 +9,7 @@ import pages as pg
 st.set_page_config(initial_sidebar_state="collapsed")
 
 pages = ["Home", "Contact"]
-styles = {
-    "nav": {
-        "background-color": "royalblue",
-        "justify-content": "left",
-    },
-    "img": {
-        "padding-right": "14px",
-    },
-    "span": {
-        "color": "white",
-        "padding": "14px",
-    },
-    "active": {
-        "background-color": "white",
-        "color": "var(--text-color)",
-        "font-weight": "normal",
-        "padding": "14px",
-    }
-}
+
 options = {
     "show_menu": False,
     "show_sidebar": False,
@@ -35,7 +17,6 @@ options = {
 
 page = st_navbar(
     pages,
-    styles=styles,
     options=options,
 )
 
