bda62f99cd96d251af2fcbb00ec9db890cb548d1
AmarantosPagiavlas2003 <a.e.pagiavlas@student.vu.nl>
2025-01-24 17:28:48 +0100

    ετστ

--- dynamic_programming/pages/visualization.py
+++ dynamic_programming/pages/visualization.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from database import get_all
+from .database import get_all
 
 # ------------------------------------------------------------------
 #                          Visualization
