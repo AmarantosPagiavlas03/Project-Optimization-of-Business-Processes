4dc6c9d96c67e3928d1167ac4e2973f4aeda3482
AmarantosPagiavlas2003 <a.e.pagiavlas@student.vu.nl>
2025-01-24 13:34:04 +0100

    test

--- dynamic_programming/pages/1_Analytics.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from streamlit_option_menu import option_menu
-from streamlit_extras.switch_page_button import switch_page
-
-st.set_page_config(page_title="Analytics", layout="wide")
-
-st.title("Analytics")
