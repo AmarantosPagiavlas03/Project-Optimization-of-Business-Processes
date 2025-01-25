24afea517ed99fb55384a74d5d7691aac1ce0526
AmarantosPagiavlas2003 <a.e.pagiavlas@student.vu.nl>
2025-01-25 01:16:52 +0100

    test

--- dynamic_programming/main.py
+++ dynamic_programming/main.py
@@ -1056,17 +1056,17 @@ def optimize_tasks_with_gurobi():
             name=f"Shift_{shift_id}_{day_str}_WorkerCap"
         )
 
-    # 6.2. Worker capacity: for each (shift, day), total nurses required
-    #     by tasks assigned cannot exceed the # of workers assigned
-    for (shift_id, day_str) in shift_worker_vars:
-        model.addConstr(
-            quicksum(
-                task_shift_vars[(t_id-1, shift_id, day_str)] + task_shift_vars[(t_id, shift_id, day_str)]
-                for (t_id, s_id, d) in task_shift_vars
-                if s_id == shift_id and d == day_str and tasks_df.loc[t_id-1, "EndTime"] <= tasks_df.loc[t_id, "StartTime"]
-            ) <= 1,
-            name=f"Shift_{shift_id}_{day_str}"
-        )
+    # # 6.2. Worker capacity: for each (shift, day), total nurses required
+    # #     by tasks assigned cannot exceed the # of workers assigned
+    # for (shift_id, day_str) in shift_worker_vars:
+    #     model.addConstr(
+    #         quicksum(
+    #             task_shift_vars[(t_id-1, shift_id, day_str)] + task_shift_vars[(t_id, shift_id, day_str)]
+    #             for (t_id, s_id, d) in task_shift_vars
+    #             if s_id == shift_id and d == day_str and tasks_df.loc[t_id-1, "EndTime"] <= tasks_df.loc[t_id, "StartTime"]
+    #         ) <= 1,
+    #         name=f"Shift_{shift_id}_{day_str}"
+    #     )
 
     # --- 7. Solve the model ---
     with st.spinner("Optimizing tasks and shifts. Please wait..."):
