45d5ff2f0aae5eb15ee59f2f94a00ae75892e7f4
= <e.arseni@student.vu.nl>
2025-01-26 04:08:09 +0200

    .

--- dynamic_programming/main.py
+++ dynamic_programming/main.py
@@ -965,7 +965,8 @@ def insert3():
             NursesRequired
         )
         VALUES
-            ('task 1', 'Monday', '09:00:00', '10:00:00', 30, 30);
+            ('task 1', 'Monday', '09:00:00', '10:00:00', 30, 20);
+            ('task 2', 'Monday', '09:00:00', '10:00:00', 30, 5);
     ''')
     conn.commit()
     c.execute('''
